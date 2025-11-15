75b9f82c0ecd15ea30734922272eafe13b990e95
Gl1tchyBoy11 <0.2poken@gmail.com>
2025-11-15 23:52:58 +0100

    Delete src/src/pages/404.json.ts

--- src/src/pages/404.json.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const get: APIRoute = () => {
-  return new Response(JSON.stringify({
-    error: 404,
-    message: "Plik nie istnieje"
-  }), { status: 404, headers: { "Content-Type": "application/json" }});
-};
